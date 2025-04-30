80bfcfe922dbca8c893a6cc7f6ac156d08d577b0
Paul Umbers <paul.umbers@gmail.com>
2025-04-30 16:07:17 -0600

    Broke up lang files

--- tailwind.config.js
+++
@@ -1,5 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  // content: ["./src/**/*.{hbs,mjs}"],
-  // safelist: ["bg-lime-100", "bg-amber-100", "bg-red-100", "bg-lime-500", "bg-amber-500", "bg-red-500"],
-};
